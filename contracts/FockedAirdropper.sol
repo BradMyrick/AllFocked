14347cf4dd7a463a9464c3e9d7c0a51163703029
BradMyrick <brad.myrick@protonmail.com>
2022-10-17 14:11:13 -0500

    first commit

---
+++ contracts/FockedAirdropper.sol
@@ -0,0 +1,17 @@
+// solidty contract that will transfer all of the tokens from an approved address to the contract address
+// the contract will then distribute the tokens to the users who are listed in the airdrop list in the most Gas efficient way possible
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract FockedAirdropper 

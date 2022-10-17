64584878485f47f8396ac4b78002ad4068ea0be0
BradMyrick <brad.myrick@protonmail.com>
2022-10-17 14:41:03 -0500

    Transfer to Airdropper contract

--- contracts/FockedAirdropper.sol
+++ contracts/FockedAirdropper.sol
@@ -13,8 +13,10 @@ import "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract FockedAirdropper is ERC721Holder {
+
+contract FockedAirdropper is ERC721Holder, Ownable {
     using Address for address;
     using Counters for Counters.Counter;
     using EnumerableSet for EnumerableSet.AddressSet;
@@ -31,12 +33,33 @@ contract FockedAirdropper is ERC721Holder {
     // the list of token ids that will be distributed
     EnumerableMap.UintToAddressMap private _tokenIds;
 
-    constructor(address _nftContractAddress, address[] memory _addresses, uint256[] memory _tokenIds) {
+    uint256[] private tokenIds;
+
+    constructor(address _nftContractAddress, address[] memory _addresses, uint256[] memory _Ids) {
         nftContractAddress = _nftContractAddress;
+        require(_addresses.length == _Ids.length, "FockedAirdropper: Address and Ids array lengths do not match");
         for (uint256 i = 0; i < _addresses.length; i++) {
             _airdropList.add(_addresses[i]);
         }
-        for (uint256 i = 0; i < _tokenIds.length; i++) {
-            _tokenIds.set(_tokenIds[i], _tokenIds[i]);
+        for (uint256 i = 0; i < _Ids.length; i++) {
+            _tokenIds.set(_Ids[i], _addresses[i]);
+        }
+        tokenIds = _Ids;
+    }
+
+    // Transfer all of the NFTs from the given address to this contract
+    function SendAllNFTs(address _from) external {
+        require(_from != address(0), "FockedAirdropper: Cannot send from the zero address");
+        require(_from != address(this), "FockedAirdropper: Cannot send from this contract");
+        require(nftContractAddress != address(0), "FockedAirdropper: NFT contract address is not set");
+        uint256 balance = IERC721(nftContractAddress).balanceOf(_from);
+        require(balance == _tokenIds.length(), "FockedAirdropper: NFT balance does not match the number of token ids");
+        for (uint256 i = 0; i < balance; i++) {
+            // sender must have approved this contract to transfer the NFTs
+            IERC721(nftContractAddress).safeTransferFrom(_from, address(this), tokenIds[i]); 
         }
     }
+
+
+
+}
---
+++ test/airdropTests.ts
@@ -0,0 +1,11 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+//TODO write tests for airdrop
+
+describe("Airdrop", function () {
+  it("should airdrop tokens to a list of addresses", async function () {
+     });
+});
